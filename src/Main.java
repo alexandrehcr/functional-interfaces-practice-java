9518ddfd5d08afc6b59f5bbd7390c6ba1de5dcc7
Alexandre Rocha <alexandrehc.rocha@gmail.com>
2023-02-05 15:03:03 -0300

    Sort by name implementing Comparable

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+
+public class Main {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+
+        List<Product> products = new ArrayList<>();
+        products.add(new Product("Smart TV", 590.00));
+        products.add(new Product("Notebook", 1900.00));
+        products.add(new Product("Keyboard", 140.00));
+        products.add(new Product("Mouse", 80.00));
+        products.add(new Product("Air Conditioning", 770.00));
+        products.add(new Product("Electric Guitar", 650.00));
+
+
+        System.out.println("====== BEFORE SORT ======");
+        for (Product product : products) {
+            System.out.println(product);
+        }
+
+        Collections.sort(products); // sort using Product's implementation of Comparable
+
+        System.out.println();
+        System.out.println("====== AFTER NAME SORT ======");
+        for (Product product : products) {
+            System.out.println(product);
+        }
+    }
+}

d70abaa0821b6b3a09dc680b89227cadfccb7406
Alexandre Rocha <alexandrehc.rocha@gmail.com>
2023-02-07 15:29:57 -0300

    Add util class implementing Function interface
    Add toUpperCaseFunction util function

---
+++ src/application/FunctionProgram.java
@@ -0,0 +1,27 @@
+package Application;
+
+import entities.Product;
+import util.toUpperCaseFunction;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
+
+public class FunctionProgram {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+
+        List<Product> products = new ArrayList<>();
+        products.add(new Product("Smart TV", 590.00));
+        products.add(new Product("Notebook", 1900.00));
+        products.add(new Product("Keyboard", 140.00));
+        products.add(new Product("Mouse", 80.00));
+        products.add(new Product("Air Conditioning", 770.00));
+        products.add(new Product("Electric Guitar", 650.00));
+
+        List<String> upperCaseProductNames = products.stream().map(new toUpperCaseFunction()).collect(Collectors.toList());
+
+        upperCaseProductNames.forEach(System.out::println);
+    }
+}

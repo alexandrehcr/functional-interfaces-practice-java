14abaeb891bc58a19e2cefe649e5411cffa686fb
Alexandre Rocha <alexandrehc.rocha@gmail.com>
2023-02-07 13:23:34 -0300

    Predicate as declared lambda expression

--- src/application/PredicateProgram.java
+++ src/application/PredicateProgram.java
@@ -1,11 +1,11 @@
 package Application;
 
 import entities.Product;
-import util.ProdPredicate;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.function.Predicate;
 
 public class PredicateProgram {
     public static void main(String[] args) {
@@ -19,8 +19,11 @@ public class PredicateProgram {
         products.add(new Product("Air Conditioning", 770.00));
         products.add(new Product("Electric Guitar", 650.00));
 
-        // Using a non-static method reference instead of a class that implements Predicate;
-        products.removeIf(Product::nonStaticPredicate);
+        double valueThreshold = 600.0;
+
+        // Using Predicate as anonymous function / declared lambda expression (must import java.util.function.Predicate)
+        Predicate<Product> predicate = p -> p.getPrice() <= valueThreshold;
+        products.removeIf(predicate);
 
         for (Product product : products) {
             System.out.println(product);
